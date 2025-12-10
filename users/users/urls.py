0e33d74f8a4bb320e0ae67e44b613aff998d544c
Purnima-Basnet04 <purnimabasnet101@gmail.com>
2025-12-10 17:50:20 +0545

    login and registers ui created

---
+++ users/users/urls.py
@@ -0,0 +1,10 @@
+# accounts/urls.py
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("register/", register, name="register"),
+    path("login", login_user, name='login'),
+    path("profile/", profile_view, name="profile"),
+    path("profile/edit/", profile_update, name="profile-update"),
+]
