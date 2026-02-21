3b05cee6ab2f5a6e17cbe216eb8447dd57df4d22
Purnima-Basnet04 <purnimabasnet101@gmail.com>
2026-02-21 22:10:55 +0545

    SMTP configured and mail system implemented for verification and job applications

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 
 # Application definition
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'skills',
     'companies',
     'applications',
+    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,3 +140,19 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+# Email Configuration (SMTP)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "WORKHUB <noreply@workhub.com>")
+
+# Email Verification Token Expiry (in hours)
+EMAIL_VERIFICATION_TOKEN_EXPIRY_HOURS = 24
+
+# OTP Settings
+OTP_EXPIRY_HOURS = 24  # OTP valid for 24 hours
+OTP_RESEND_COOLDOWN_SECONDS = 60  # Cannot resend OTP within 1 minute
+"" 
